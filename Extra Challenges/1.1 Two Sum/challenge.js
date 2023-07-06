36fba813d5d7ce40cbb6a92521ddd4208a953796
Roy Jumah <royjumah17@gmail.com>
2023-07-06 16:54:56 +0300

    optimize solution with two pointer approach

--- Extra Challenges/1.1 Two Sum/challenge.js
+++ Extra Challenges/1.1 Two Sum/challenge.js
@@ -1,9 +1,20 @@
-const twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+const TwoSum = (nums, target) => {
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      return [nums[left], nums[right]];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
     }
   }
+  return [];
 };
+console.log(TwoSum([1,3,4,5],2))
+console.log(TwoSum([1,3,4,5],9))
