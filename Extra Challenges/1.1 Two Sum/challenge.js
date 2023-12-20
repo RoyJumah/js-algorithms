b0386b76d7570cf5dcf649f02e5fabca5204d1b0
RoyJumah <royjumah17@gmail.com>
2023-12-20 09:17:54 +0300

    add map approach to two sum challenge

--- Extra Challenges/1.1 Two Sum/challenge.js
+++ Extra Challenges/1.1 Two Sum/challenge.js
@@ -1,20 +1,35 @@
 const TwoSum = (nums, target) => {
-  nums.sort((a, b) => a - b);
-  let left = 0;
-  let right = nums.length - 1;
+  //Two Sum Approach
+  // nums.sort((a, b) => a - b);
+  // let left = 0;
+  // let right = nums.length - 1;
+  // while (left < right) {
+  //   const sum = nums[left] + nums[right];
+  //   if (sum === target) {
+  //     return [nums[left], nums[right]];
+  //   } else if (sum < target) {
+  //     left++;
+  //   } else {
+  //     right--;
+  //   }
+  // }
+  // return [];
+  //Map Approach
 
-  while (left < right) {
-    const sum = nums[left] + nums[right];
+  const map = new Map();
 
-    if (sum === target) {
-      return [nums[left], nums[right]];
-    } else if (sum < target) {
-      left++;
-    } else {
-      right--;
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
     }
+
+    map.set(nums[i], i);
   }
   return [];
 };
-console.log(TwoSum([1,3,4,5],2))
-console.log(TwoSum([1,3,4,5],9))
+
+console.log(TwoSum([1, 3, 4, 5], 9));
+
+// 9-1 = 8 (1, 0), 3, 1, 4, 2,
